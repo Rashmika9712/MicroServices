e8f319fc718516a3c71c9eced62cd4aa0ece45d7
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-12-10 18:52:53 +0530

    basket controller

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,27 @@
+using Basket.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    var configuration = builder.Configuration.GetSection("CacheSettings");
+    options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+#region Services
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+#endregion
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        private readonly IConfiguration _configuration;
+
+        public BasketRepository(IDistributedCache redisCache, IConfiguration configuration)
+        {
+            _redisCache = redisCache ?? throw new ArgumentException(nameof(redisCache));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache?.GetStringAsync(userName);
+
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
